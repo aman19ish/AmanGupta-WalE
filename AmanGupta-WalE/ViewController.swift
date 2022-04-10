889df5b43c42df324a44fe6298310ac4e9a3497f
aman gupta <aman.gupta@dianapps.com>
2022-04-10 11:41:46 +0530

    Create New Project

---
+++ AmanGupta-WalE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmanGupta-WalE
+//
+//  Created by Aman gupta on 10/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
